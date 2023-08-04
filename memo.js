21502fb9787a2396679eb28432806cee94ac9f9c
YongBin123 <37dnfqks@naver.com>
2023-08-04 21:25:31 +0900

    seven commit

--- memo.js
+++ memo.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   // 페이지가 로드될 때 서버에서 메모 가져오기
-  fetch('/getMemos')
+  fetch('/getMemos') // 서버에 '/getMemos' 경로로 요청을 보내서 메모를 가져옴
     .then((response) => response.json()) // 서버 응답은 json 형식으로
     .then((memos) => { // json 데이터 처리
       memos.forEach((memo) => {
@@ -52,12 +52,12 @@ function createMemoElement(memo) {
   deleteButton.textContent = "삭제";
   deleteButton.onclick = function () {
     // 삭제 버튼 클릭 시 메모를 삭제하고 로컬 스토리지에 반영
-    var savedMemos = JSON.parse(localStorage.getItem('memos') || '[]');
+    var savedMemos = JSON.parse(localStorage.getItem('memos') || '[]'); // 로컬 스토리지에서 'memos' 키에 저장된 데이터를 가져옴
     // 삭제할 메모의 인덱스를 찾기 위해 배열 내에서 해당 메모의 인덱스를 찾음
     var index = savedMemos.findIndex((item) => item.content === memo.content);
     // 인덱스가 유효한 경우 (메모가 존재하는 경우) 해당 메모를 배열에서 제거
     if (index !== -1) {
-      savedMemos.splice(index, 1);
+      savedMemos.splice(index, 1); // 해당 인덱스에 위치한 메모 하나를 배열에서 제거
       // 변경된 메모 배열을 다시 로컬 스토리지에 'memos' 키로 저장하여 변경 내용 반영
       localStorage.setItem('memos', JSON.stringify(savedMemos));
     }
@@ -87,7 +87,7 @@ function saveMemo() {
 
     // 서버로 메모 저장 요청 보내기
     fetch('/saveMemo', {
-      method: 'POST',
+      method: 'POST', // 서버에 데이터를 제출
       headers: {
         'Content-Type': 'application/json',  // 요청 본문의 데이터가 JSON 형식임을 서버에 알림
       },
@@ -128,7 +128,7 @@ function saveMemo() {
 }
 
 function saveMemoToLocalStorage(memo) {
-  const savedMemos = JSON.parse(localStorage.getItem('memos') || '[]');
+  const savedMemos = JSON.parse(localStorage.getItem('memos') || '[]');  // 만약 'memos' 키에 저장된 데이터가 없으면 빈 배열로 초기화
   savedMemos.push(memo);
   localStorage.setItem('memos', JSON.stringify(savedMemos)); // savedMemos 배열을 JSON 문자열로 변환한 뒤, 'memos' 키에 해당 문자열을 로컬 스토리지에 저장
 }
